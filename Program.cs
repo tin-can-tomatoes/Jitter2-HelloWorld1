3ed99178eff8e264db7ac13968190825eea011d5
tomatoes <tomatoes@tin-can.ca>
2024-09-07 06:58:34 -0400

    Another test

--- Program.cs
+++ Program.cs
@@ -1,11 +1,54 @@
-﻿using Jitter2;
+﻿Console.WriteLine("Hello, World!");
 
+SpinUpThreads();
 
-World world = new(numBodies: 1, numContacts: 1, numConstraints: 1);
+Console.WriteLine("Bye, World!");
 
+void SpinUpThreads()
+{
+    void ThreadProc()
+    {
+        Thread.Yield();
+    }
 
-world.Step(1f/100f, false);
-world.Step(1f/100f, false);
-world.Step(1f/100f, false);
+    var threads = new Thread[8];
 
-Console.WriteLine("Foo");
+    Console.WriteLine("Initializing.");
+
+    var initWaitHandle = new AutoResetEvent[8]{
+        new AutoResetEvent(false),
+        new AutoResetEvent(false),
+        new AutoResetEvent(false),
+        new AutoResetEvent(false),
+        new AutoResetEvent(false),
+        new AutoResetEvent(false),
+        new AutoResetEvent(false),
+        new AutoResetEvent(false)
+    };
+
+    for (int i = 0; i < 6; i++)
+    {
+        int local_i = i;
+        threads[i] = new Thread(() =>
+            {
+                Console.WriteLine($"Started thread {local_i}");
+              //  Thread.Sleep(3);
+                initWaitHandle[local_i].Set();
+                //Console.WriteLine($"About to sleep thread {local_i}");
+               // Thread.Sleep(3);
+                Console.WriteLine($"About to enter loop thread {local_i}");
+                while (true)
+                {
+                     Thread.Yield();
+                }
+            });
+
+        threads[i].IsBackground = false;
+        threads[i].Start();
+        //Thread.Yield();
+    }
+    for(int i = 0; i<6;i++){
+        Console.WriteLine($"Waiting on thread {i}");
+        initWaitHandle[i].WaitOne();
+    }
+}
